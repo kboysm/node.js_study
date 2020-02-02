a6556ddd5b350bd8c74cac66e6ee1c51dc66a9d2
kboysm <lishal12@naver.com>
2020-02-02 21:03:31 +0900

    fs

--- node.js_file_demo/app_file.js
+++ node.js_file_demo/app_file.js
@@ -1,5 +1,8 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let fs = require('fs');
 let app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.locals.pretty=true;
 app.set('views','./views');
 app.set('view engine','jade');
@@ -7,7 +10,14 @@ app.get('/topic/new',(req,res)=>{
     res.render('new');
 });
 app.post('/topic',(req,res)=>{
-    res.send('topic hi')
+    let title = req.body.title;
+    let description = req.body.description;
+    fs.writeFile('data/'+title,description,(err)=>{
+        if(err){
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success');
+    });
 })
 app.listen(3000,()=>{
     console.log("Connected , 3000 Port");
