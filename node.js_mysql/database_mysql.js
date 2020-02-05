dca478c93eba5dca56b8bfa5feafc6983778cd4b
kboysm <lishal12@naver.com>
2020-02-05 23:21:58 +0900

    nodejs_mysql_update&delete

--- node.js_mysql/database_mysql.js
+++ node.js_mysql/database_mysql.js
@@ -18,13 +18,31 @@ connection.query(sql,(err,rows,fields)=>{
     }
 });
 */
-let sql = 'insert into topic (title,description,author) values(?,?,?)';
-let params=['Supervisor','Watcher','graphittie'];
+// let sql = 'insert into topic (title,description,author) values(?,?,?)';
+// let params=['Supervisor','Watcher','graphittie'];
+// connection.query(sql,params,(err,rows,fields)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(rows.insertId);
+//     }
+// });
+// let sql = 'update topic set title=?, author=? where id=?';
+// let params=['NPM','leezcheck',2];
+// connection.query(sql,params,(err,rows,fields)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(rows);
+//     }
+// });
+let sql = 'delete from topic where id=?';
+let params=[1];
 connection.query(sql,params,(err,rows,fields)=>{
     if(err){
         console.log(err);
     }else{
-        console.log(rows.insertId);
+        console.log(rows);
     }
 });
 connection.end();
