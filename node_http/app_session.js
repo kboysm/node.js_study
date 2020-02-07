db353a04aa3a5617584a781a6bd7c11346315bb7
kboysm <lishal12@naver.com>
2020-02-08 06:25:20 +0900

    session_login

--- node_http/app_session.js
+++ node_http/app_session.js
@@ -15,6 +15,23 @@ app.get('/count',(req,res)=>{
     }
     res.send('count : '+req.session.count);
 });
+app.get('/auth/login',(req,res)=>{
+    let output=`
+    <form action="/auth/login" method="post">
+        <p>
+            <input type="text" name="id" placeholder="username">
+        </p>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+            <input type="submit" value="제출">
+        </p>
+    </form>
+    `;
+    res.send('hello login');
+});
+
 app.listen(3003,()=>{
     console.log('Connected 3003 port!!!');
 });
