6e8614299a2fb4ace5aa7ce5b69b7d23fced4a9e
kboysm <lishal12@naver.com>
2020-02-07 19:41:18 +0900

    cookies_cart

--- node_http/app_cookie.js
+++ node_http/app_cookie.js
@@ -2,6 +2,28 @@ let express = require('express');
 let cookieParser = require('cookie-parser');
 let app = express();
 app.use(cookieParser()); //app에서 쿠키파써를 사용!
+let products={
+    1:{title:'The history of web 1'},
+    2:{title:'The next web'}
+};//db대신 배열을 만듬 , 실무에서는 이부분이 db라고 생각하면 된다.
+app.get('/products',(req,res)=>{
+    let output = '';
+    for(let name in products){
+        output +=`<li>
+        <a href="/cart/${name}">${products[name].title}</a>
+        </li>`;
+    }
+    res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
+});
+app.get('/cart/:id',(req,res)=>{
+    let id = req.params.id;
+    res.send('h'+id);
+});
+
+
+
+
+
 app.get('/count',(req,res)=>{
     let count;
     if(req.cookies.count){
