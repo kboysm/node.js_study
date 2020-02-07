b6496b3dcd8e095dc523b873b02bb16841b7a461
kboysm <lishal12@naver.com>
2020-02-07 21:19:02 +0900

    cookies_security

--- node_http/app_cookie.js
+++ node_http/app_cookie.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let app = express();
-app.use(cookieParser()); //app에서 쿠키파써를 사용!
+app.use(cookieParser('51615651SDSA!@#$')); //app에서 쿠키파써를 사용! ,cookieParser에 암호인자를 넣어 쿠키를 암호화 시킬 수 있다.
 let products={
     1:{title:'The history of web 1'},
     2:{title:'The next web'}
@@ -16,7 +16,7 @@ app.get('/products',(req,res)=>{
     res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
 });
 app.get('/cart',(req,res)=>{
-    let cart = req.cookies.cart;
+    let cart = req.signedCookies.cart;
     let output='';
     if(!cart){
         res.send('Empty');
@@ -34,8 +34,8 @@ app.get('/cart',(req,res)=>{
 app.get('/cart/:id',(req,res)=>{
     let id = req.params.id;
     let cart;
-    if(req.cookies.cart){
-        cart =req.cookies.cart;
+    if(req.signedCookies.cart){
+        cart =req.signedCookies.cart;
     }else{
         cart ={};
     }
@@ -43,7 +43,7 @@ app.get('/cart/:id',(req,res)=>{
         cart[id]=0; 
     }
     cart[id]=parseInt(cart[id])+1; 
-    res.cookie('cart',cart);
+    res.cookie('cart',cart,{signed:true});
     res.redirect('/cart');
 });
 /*
@@ -57,17 +57,22 @@ cart={
 
 app.get('/count',(req,res)=>{
     let count;
-    if(req.cookies.count){
-         count = parseInt(req.cookies.count);
+    if(req.signedCookies.count){
+         count = parseInt(req.signedCookies.count); //signedCookies ->Cookie를 암호화시킴 ,cookieParser('51615651SDSA!@#$')키값으로 지정해 놔서 가능
     }
     else{
          count =0;
     }
     count=count+1;
-    res.cookie('count',count); //쿠키이름 , default값
+    res.cookie('count',count,{signed:true}); //쿠키이름 , default값
     res.send('count : '+count);
 });
 app.listen(3003,()=>{
     console.log('Connected 3003 port!!!');
 });
 //express는 쿠키관련 기능을 기본적으로 제공하지 않아서 깔아야 한다. ->npm i cookie-parser
+//cookie는 사용자의 웹브라우저에 저장되므로 해킹이 될 가능성이 높다! 위험하다!
+/*
+그래서 https를 사용하는게 좋다.
+또 쿠키값 자체를 암호화를 할 수 있다. 
+ */
