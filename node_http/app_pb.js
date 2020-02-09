4c6576f13fd4ee6553d42965465025732aa450fd
kboysm <lishal12@naver.com>
2020-02-09 17:48:35 +0900

    register

--- node_http/app_pb.js
+++ node_http/app_pb.js
@@ -19,6 +19,13 @@ app.use(session({
             database: 'o2'
     }) //file과 달리 DB사용시 반드시 option을 넣어줘야함 ,이 옵션을 주고 app을 실행하면 자동으로 DB에 세션이라는 테이블이 생성됌
   }));
+let user = 
+[{
+    username:'Lsm',
+    password:'8Y5Hg2OnCv8nJgCs2Mo+c8MYCFcedZxC6TUmy7W5kMPY+O0NKOO+bcjh5ofUAIF9SlpD9af36WkqqLlPcUSydVntnhXSkHM6wx+cmOOPEWZ7V3/MgaSwSSiNEwYhnBoQ9QtG/DLnuIYcD7004RbdKLbK856Hl24RVUl1cPczVPo=',
+    salt : 'J5qJREh1sj68/Bbj+ZJaxc1Y1QKAPpy18UKFUO2rkgH1S8up39lLRUqx9nnmpfGa5uKlCPp3diG11H/KBgY2oQ==', //사용자의 각각의 salt값을 다르게하면 설령 비번이 동일한 사용자가 있더라도 password의 값이 다르다
+    displayName:'nickNameLSM'
+}];
 app.get('/count',(req,res)=>{
     if(req.session.count){
         req.session.count++;
@@ -35,13 +42,6 @@ app.get('/auth/logout',(req,res)=>{
 });
 
 app.post('/auth/login',(req,res)=>{
-    let user = 
-    {
-        username:'Lsm',
-        password:'8Y5Hg2OnCv8nJgCs2Mo+c8MYCFcedZxC6TUmy7W5kMPY+O0NKOO+bcjh5ofUAIF9SlpD9af36WkqqLlPcUSydVntnhXSkHM6wx+cmOOPEWZ7V3/MgaSwSSiNEwYhnBoQ9QtG/DLnuIYcD7004RbdKLbK856Hl24RVUl1cPczVPo=',
-        salt : 'J5qJREh1sj68/Bbj+ZJaxc1Y1QKAPpy18UKFUO2rkgH1S8up39lLRUqx9nnmpfGa5uKlCPp3diG11H/KBgY2oQ==', //사용자의 각각의 salt값을 다르게하면 설령 비번이 동일한 사용자가 있더라도 password의 값이 다르다
-        displayName:'nickNameLSM'
-    }
     //현시점에서 암호화는 sha256을 사용함
     ; //DB대신 직접박음 , 소스코드에 비번이 있는경우 굉장히 안좋은 방식
     let uname=req.body.username;
@@ -58,6 +58,42 @@ app.post('/auth/login',(req,res)=>{
             }
         });
     }
+});
+app.post('/auth/register',(req,res)=>{
+    hasher({password:req.body.password},(err,pass,salt,hash)=>{
+        let uu = {
+            username:req.body.username,
+            password:hash,
+            salt:salt,
+            displayName:req.body.displayName
+        };
+        user.push(uu);
+        req.session.displayName = req.body.displayName;
+        req.session.save(()=>{
+            res.redirect('/welcome');
+        });
+    });
+
+});
+app.get('/auth/register',(req,res)=>{
+    let output=`
+    <h1>Register</h1>
+    <form action="/auth/register" method="post">
+        <p>
+        <input type="text" name="username" placeholder="username">
+        </p>
+        <p>
+        <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+        <input type="text" name="displayName" placeholder="nickName">
+        </p>
+        <p>
+        <input type="submit">
+        </p>
+    </form>
+    `;
+    res.send(output);
 });
 app.get('/welcome',(req,res)=>{
     if(req.session.displayName){
