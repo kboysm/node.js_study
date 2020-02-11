18612759b71ab991daa9b6017679bd01e7d8fb8a
kboysm <lishal12@naver.com>
2020-02-12 05:22:35 +0900

    ch02_final

--- Do it node.js 프로그래밍/ch02_test6.js
+++ Do it node.js 프로그래밍/ch02_test6.js
@@ -2,5 +2,6 @@ let cal = require('./calc2');// 객체 자체를 받음
 console.log('모듈로 분리한 후 : cal2.add : '+cal.add(10,10));
 
 let nconf=require('nconf');
+nconf.env();
 let value = nconf.get('OS');
 console.log("os 환경변수의 값 : "+value);
